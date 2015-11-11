8ea22c8e7095f5cca7ef221a178c72592ef280b9
Peter Jang <peter.x.jang@gmail.com>
2015-11-10 21:31:24 -0600

    added a view for all recipes

--- app/models/recipe.rb
+++ app/models/recipe.rb
@@ -1,2 +1,19 @@
 class Recipe < ActiveRecord::Base
+  attr_accessor :title, :ingredients, :chef, :directions
+  
+  def title_in_caps
+    title.upcase
+  end
 end
+
+
+
+# class Dog
+#   def bark
+#     "woof"
+#   end
+
+#   def bark_twice
+#     bark * 2
+#   end
+# end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   get '/' => 'recipes#home'
+  get '/all' => 'recipes#all'
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
