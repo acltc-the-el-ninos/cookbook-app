86fb684d215b6a8e7529ddd97b4dd8d452f79c4f
Peter Jang <peter.x.jang@gmail.com>
2015-11-17 21:34:00 -0600

    added model methods for making lists of ingredients and directions, and formatting time

--- app/models/recipe.rb
+++ app/models/recipe.rb
@@ -2,4 +2,16 @@ class Recipe < ActiveRecord::Base
   def title_in_caps
     title.upcase
   end
+
+  def ingredients_list
+    ingredients.split(", ")
+  end
+
+  def directions_list
+    directions.split(". ")
+  end
+
+  def readable_created_at
+    created_at.strftime("%B %e, %Y")
+  end
 end
