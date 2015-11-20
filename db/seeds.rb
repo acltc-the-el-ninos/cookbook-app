b0dff408eeb4029fb92220bc7f0468bd37abd519
Peter Jang <peter.x.jang@gmail.com>
2015-11-20 13:59:07 -0600

    converted sqlite to mysql

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,8 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
-#
-# Examples:
-#
-#   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
-#   Mayor.create(name: 'Emanuel', city: cities.first)
+Recipe.create!([
+  {title: "Raw eggs!", chef: "Jay Wengrow", ingredients: "1 live chicken", directions: "Squeeze the chicken", image: "http://www.chatelaine.com/wp-content/uploads/2013/04/Eggs-May-13-p28.jpg", prep_time: 100},
+  {title: "Empty sandwich", chef: "Jay Wengrow", ingredients: "2 slices of bread", directions: "Put the bread together", image: "http://www.levisiondesign.com/img/empty-sandwich.jpg", prep_time: 120},
+  {title: "Ice cubes!!!", chef: "Nina Leung", ingredients: "Water", directions: "Freeze.", image: "http://d1oi7t5trwfj5d.cloudfront.net/8b/7cc5d01fb411e18f3c123138165f92/file/Ice-Cube_420-420x0.jpg", prep_time: 36},
+  {title: "quartz!", chef: "who", ingredients: "everything", directions: "run.", image: "http://www.healingwithcrystals.net.au/uploads/1/5/5/7/15572098/430425869_orig.jpg?210", prep_time: 432},
+  {title: "Buttered eel", chef: "Josh Nixon", ingredients: "1 eel, 1 stick of butter", directions: "Feed stick of butter to live eel. Microwave.", image: "http://cdn-jpg.thedailymeal.net/sites/default/files/styles/channel_eat_a4_teaser/public/story/butter-sushi_0.JPG?itok=Elpe-KO6", prep_time: nil},
+  {title: "Killa Pizza", chef: "Peter Jang", ingredients: "Dough, shrimp, cheese, pineapple, sausages, tomato sauce, ricotta cheese, feta cheese, jalepenos", directions: "Knead the dough. Spread the tomato sauce. Add shrimp first. Microwave. Add rest of ingredients. Let sit in the sun for 3 hours.", image: "http://vignette3.wikia.nocookie.net/spaceballs/images/c/c4/Pizza_the_Hutt.jpg/revision/latest?cb=20131101150125", prep_time: nil}
+])
